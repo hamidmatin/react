8658f6307eeec2c53315428f5065c9b0287e5d42
Hamid Reza Izadi Matin <hamid.matin@gmail.com>
2020-09-11 12:49:53 +0330

    Router

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import About from "./About";
+import Contact from "./Contact";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import "./Nav.css";
+
 
 export default class Nav extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+      const activeStyle ={
+          color: 'red'
+      }
+    return (
+      <div>
+        <BrowserRouter>
+          {/* <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
+          <NavLink to="/contact" activeStyle={activeStyle}>Contact</NavLink> */}
+          {/* <NavLink to="/about" >About</NavLink>
+          <NavLink to="/contact" >Contact</NavLink> */}
+          <NavLink to="/about" activeClassName="custom-active">About</NavLink>
+          <NavLink to="/contact" activeClassName="custom-active">Contact</NavLink>
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
--- src/Containers/Blog/Blog.js
+++ src/Containers/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Users from '../../Components/Users/Users';
-
+import Nav from '../../Components/Nav/Nav'
 export default function Blog() {
     return (
         <div>
+            <Nav/>
             <Users />
         </div>
     )
