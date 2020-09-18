f82688ca732f67afe99bd47e62161585e554b0ac
Hamid Reza Izadi Matin <hamid.matin@gmail.com>
2020-09-18 12:59:09 +0330

    HOC

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -1,29 +1,42 @@
-import React from "react";
-import {
-  BrowserRouter,
-  Link,
-  Route,
-  Switch,
-  useRouteMatch,
-} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
 import WebDesign from "./WebDesign";
 import MobileApp from "./MobileApp";
 import WindowsApp from "./WindowsApp";
-export default function Services() {
-  let { path, url } = useRouteMatch();
-  return (
-    <div>
-      <h1>Services</h1>
-      <p>akjdsf akjdfg adfkg adkghf</p>
-      <Link to={`${url}/webdesign`}>Web Design</Link>
-      <Link to={`${url}/mobileapp`}>Mobile</Link>
-      <Link to={`${url}/windowsapp`}>Windows</Link>
+let { path, url } = useRouteMatch();
+const [isLoading, setIsLoading] = useState(true);
+// let Content = null;
 
-      <Switch>
-        <Route path={`${path}/webdesign`} component={WebDesign} />
-        <Route path={`${path}/mobileapp`} component={MobileApp} />
-        <Route path={`${path}/windowsapp`} component={WindowsApp} />
-      </Switch>
-    </div>
-  );
+const [content, setContent] = useState(null);
+
+useEffect(() => {
+  setTimeout(() => {
+    setContent( (
+      <>
+        <h1>Services</h1>
+        <p>akjdsf akjdfg adfkg adkghf</p>
+        {/* <Link to={`${url}/webdesign`}>Web Design</Link>
+        <Link to={`${url}/mobileapp`}>Mobile</Link>
+        <Link to={`${url}/windowsapp`}>Windows</Link>
+
+        <Switch>
+          <Route path={`${path}/webdesign`}>
+            <WebDesign />
+          </Route>
+          <Route path={`${path}/mobileapp`} component={MobileApp} />
+          <Route path={`${path}/windowsapp`} component={WindowsApp} />
+        </Switch> */}
+      </>
+    ));
+    console.log(content);
+    setIsLoading(false)
+  }, 3000);
+});
+
+export default function Services() {
+  
+
+  return <div>
+    {content}
+  </div>;
 }
